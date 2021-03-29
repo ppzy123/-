0b95c4fe6a2ef02b4679b7e2409f6d7f7d317580
ppzy123 <1157928777@qq.com>
2021-03-29 09:34:06 +0800

    write models and rebuild sql

--- web_paper/static/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.writeln("<nav>");
-document.writeln("<li><a href=\"index.html\">快爬</a></li>");
-document.writeln("<li><a href=\"test.html\">论文列表</a></li>");
-document.writeln("<li><a href=\"\">论文操作</a></li>");
-document.writeln("<li><a href=\"\">热门领域</a></li>");
-document.writeln("<li class=\"right\"><a href=\"\">关于我们</a></li>");
-document.writeln("<li class=\"right\"><a href=\"login.html\">登录</a></li>");
-document.writeln("</nav>");
--- web_paper/text.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, flash
-from flask import request
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
